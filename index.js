eb63a07c0f79d6cfede076fbb4b154503eddb4c0
SidneyKing <sid8121@gmail.com>
2023-07-30 22:08:00 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+main.remove()
+
+const newHeader = document.createElement("h1")
+
+//document.body.append(newHeader) 
+
+newHeader.id = "victory"
+
+newHeader.textContent = "Sid is the champion"
+
+
+
+
+
+
+
+
